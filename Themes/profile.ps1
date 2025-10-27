8192ee3f1fbb5d77d0fef04a9e933eaadabfe9fd
Richard Minder <112668083+minder-security@users.noreply.github.com>
2025-10-27 20:59:52 +0100

    Create profile.ps1

---
+++ Themes/profile.ps1
@@ -0,0 +1,2 @@
+# oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/catppuccin_mocha.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/dracula.omp.json" | Invoke-Expression
